37478a2df58f7b97e157733bff94616fcbce5613
Bosun <beliy-81@mail.ru>
2023-12-20 15:23:12 +0300

    update project

---
+++ hexlet-laravel-blog/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+
+class ArticleController extends Controller
+{
+    public function index(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
+    {
+        $articles = Article::paginate(5);
+
+        // Статьи передаются в шаблон
+        // compact('articles') => [ 'articles' => $articles ]
+        return view('article.index', compact('articles'));
+    }
+}
--- hexlet-laravel-blog/routes/web.php
+++ hexlet-laravel-blog/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,3 +32,8 @@ Route::get('/', function () {
 
 Route::get('about', [PageController::class, 'about']);
 //Route::get('team', [PageController::class, 'team']);
+
+// Название сущности в URL во множественном числе, контроллер в единственном
+Route::get('articles', [ArticleController::class, 'index'])
+    ->name('articles.index'); // имя маршрута, нужно для того, чтобы не создавать ссылки руками
+
